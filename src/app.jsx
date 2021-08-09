57a05805ef04a480529d377b15a932824625af3b
Wood-Jack <thewoodrowz@gmail.com>
2021-08-09 13:55:13 -0400

    adding all changes into docs so far

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,48 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+
+  constructor(props){
+
+    super(props);
+
+    this.state= {
+
+      topspots:[]
+    };
+  }
+
+  componentWillMount(){
+
+    axios
+
+    .get('https://origin-top-spots-api.herokuapp.com/api/topspots')
+    .then(response => response.data)
+    .then(topspots => this.setState({ topspots}));
+
+  }
+
+
   render() {
+    <pre>
+      {JSON.stringify(this.state.topspots, null, 2)}
+    </pre>
+
+
     return (
-      <div className='App'></div>
+      <div className='App'>
+        <div className='container'>
+          <h1>San Diego Top Spots</h1>
+          <h3>A list of the top 30 places to see in San Diego, California.</h3>
+
+
+
+
+        </div>
+
+
+
+      </div>
     );
   }
 }
---
+++ src/topspot.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default props => (
+
+    <div className='well'>
+        <h4>{props.name}</h4>
+        <p>{props.description}</p>
+    </div>
+);
